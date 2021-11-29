6f0e8ad2a16e87a284ca863b05461342a442426d
phenriquep00 <phenriquep00@gmail.com>
2021-11-29 11:11:25 -0300

    added files

---
+++ poker/deck.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+
+class Deck:
+
+    def __init__(self):
+        self.clubs = [Card(image=f'{_}.png', value=_, suit='clubs') for _ in range(2, 15)]
+        self.diamonds = [Card(image=f'{_}.png', value=_, suit='diamonds') for _ in range(2, 15)]
+        self.hearts = [Card(image=f'{_}.png', value=_, suit='hearts') for _ in range(2, 15)]
+        self.spades = [Card(image=f'{_}.png', value=_, suit='spades') for _ in range(2, 15)]
+
+
+class Card:
+    # Each card will have a image attribute (from pics) and a value
+    def __init__(self, image, value, suit):
+        self.image = pygame.image.load(os.path.join('pics', f'{suit}', f'{image}'))
+        self.value = value
+        self.suit = suit
---
+++ poker/main.py
@@ -0,0 +1,9 @@
+import pygame
+import deck
+
+X = 400
+Y = 400
+
+deck = deck.Deck()
+display_surface = pygame.display.set_mode((X, Y))
+
