ffd6766568899f7894be6532c07dd5c11bb192fc
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-31 14:55:41 -0300

    added Button class

---
+++ poker/Classes/Buttons/buttons.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Button:
+    def __init__(self, surf, color, X, Y, rectW, rectH):
+        self.surf = surf
+        self.color = color
+        self.X = X
+        self.Y = Y
+        self.rectW = rectW
+        self.rectH = rectH
+
+    def draw(self):
+        pygame.draw.rect(
+            surface=self.surf,
+            color=self.color,
+            rect=[
+                (self.X, self.Y),
+                (self.rectW, self.rectH)
+            ],
+            border_radius=5
+        )
--- poker/main.py
+++ poker/main.py
@@ -1,6 +1,8 @@
 import os
 import pygame
 
+from Classes.Buttons.buttons import Button
+
 
 # functions:
 # function to rotate an image object around it's center
@@ -13,13 +15,30 @@ def blit_rotate_center(surf, image, topleft, angle):
 
 # core pygame configuration
 WIDTH, HEIGHT = 901, 600  # window size
-window = pygame.display.set_mode(WIDTH, HEIGHT)  # display object
+window = pygame.display.set_mode([WIDTH, HEIGHT])  # display object
 pygame.display.set_caption("Pypoker")   # game caption change
 fps = 60    # frames per second
 timer = pygame.time.Clock()     # timer object
 
+# image
+background = pygame.image.load(os.path.join('pics', 'poker_background.jpeg'))
+
+
+# components
+play = Button(window, (0, 255, 0), 200, 100, 200, 200)
 
 run = True
 
 while run:
+
+    window.blit(background, (0, 0))
+    play.draw()
+
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
     timer.tick(fps)
+
