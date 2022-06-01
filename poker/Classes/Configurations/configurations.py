159524ea5e65304a49aec7810607fdda9a5a5e66
Pedro Lima <contact.pedrolima@gmail.com>
2022-06-01 16:47:58 -0300

    Configurations documentation

--- poker/Classes/Configurations/configurations.py
+++ poker/Classes/Configurations/configurations.py
@@ -4,18 +4,27 @@ from poker.Classes.Buttons.buttons import Button
 
 class Configurations:
     def __init__(self, surface):
+        """
 
+        :param surface: pygame display object to be the surface where the configuration object will be displayed
+        """
         self.surface = surface
         self.active = False
         self.back_button = Button(self.surface, COLOR.dark_gray3, 0, 0, 100, 100, 'Back', 'm')
 
     def draw(self):
+        """
+        fill the surface screen with the configs background, and draw the elements inside configrations
+        :return:
+        None
+        """
         self.surface.fill(COLOR.black)
         self.back_button.draw()
 
     def toggle_config(self):
         """
-        change the active attribute to True to allow the config window to be initialized
+        toggle the configuration object attribute active to whether draw or not the configuration object over the
+        surface
         :return:
         None
         """
