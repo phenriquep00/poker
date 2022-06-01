610e4774a3975523683b2e7e8f2fea044d960f4b
Pedro Lima <contact.pedrolima@gmail.com>
2022-06-01 16:53:14 -0300

    Player documentation

--- poker/Classes/Player/player.py
+++ poker/Classes/Player/player.py
@@ -1,5 +1,9 @@
 class Player:
     def __init__(self, name):
+        """
+        Player objects to gather all player's data and actions
+        :param name: player's name
+        """
         self.hand = []
         self.chips = 500
         self.name = name
