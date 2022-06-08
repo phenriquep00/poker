5bc4ac0079582b21cf30455f5b4dc227ca0e0bb5
Pedro Lima <contact.pedrolima@gmail.com>
2022-06-08 15:11:02 -0300

    added flush catcher

--- poker/Classes/Analyzer/win_analyzer.py
+++ poker/Classes/Analyzer/win_analyzer.py
@@ -18,7 +18,37 @@ class WinAnalyzer:
     def __get_utils(card):
         return card.value, card.suit
 
+    def evaluate(self, hand):
+        h, s, d, c = [], [], [], []
+        for card in hand:
+            # get amount of cards in every suit
+            if card[1] in 'hearts':
+                h.append(card)
+            if card[1] in 'spades':
+                s.append(card)
+            if card[1] in 'diamonds':
+                d.append(card)
+            if card[1] in 'clubs':
+                c.append(card)
+
+            # flush possibilities
+            for suit in [h, s, d, c]:
+                if len(suit) >= 5:    # if true, there's a flush
+                    # sorting
+                    cards = sorted([suit[_][0] for _ in range(len(suit))], reverse=True)
+                    # royal flush
+                    if sum(cards[:5]) >= 60:
+                        # if true, there's a royal flush
+                        return 'royal flush'
+                    # straight flush
+                    elif cards[0] - 4 == cards[4]:
+                        return 'straight flush'
+                    else:
+                        # flush
+                        return 'flush'
+
 
 game = TestGame('surface')
 an_test = WinAnalyzer(game.table.cards, game.players)
-print(an_test.hands['Player'])
+test_hand = [(2, 'diamonds'), (3, 'diamonds'), (4, 'diamonds'), (5, 'diamonds'), (6, 'diamonds')]
+print(an_test.evaluate(test_hand))
